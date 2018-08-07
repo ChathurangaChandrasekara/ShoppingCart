2e338565f17db564d9909097bf1ecb185eb8065d
ChathurangaChandrasekara <chathurangajanitha67@gmail.com>
2018-08-07 20:27:49 +0530

    initialize project

---
+++ ShoppingCart/Models/ShoppingCartDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Models
+{
+    public class ShoppingCartDbContext : DbContext
+    {
+        public ShoppingCartDbContext(DbContextOptions<ShoppingCartDbContext> options) : base(options)
+        { }
+
+
+    }
+}
