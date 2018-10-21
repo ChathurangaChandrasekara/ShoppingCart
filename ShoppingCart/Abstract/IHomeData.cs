61b8d3e18678443cbf2ad1aa2210f05b91d96e6b
ChathurangaChandrasekara <chathurangajanitha67@gmail.com>
2018-10-21 23:04:53 +0530

    modify some files

---
+++ ShoppingCart/Abstract/IHomeData.cs
@@ -0,0 +1,18 @@
+﻿using ShoppingCart.Areas.Shop.Models;
+using ShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Abstract
+{
+    public interface IHomeData
+    {
+        List<ItemDTO> ItemListData1(int id); 
+        List<ItemDTO> ItemListData2(int id);
+        List<ItemDTO> ItemListData3(int id);
+        List<ItemDTO> ItemListData4(int id);
+        List<ItemDTO> ItemListData5(int id);
+    }
+}
