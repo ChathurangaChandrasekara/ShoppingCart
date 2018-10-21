93e41ca62691c3e0e4508a21fd9cb860041f6f6e
ChathurangaChandrasekara <chathurangajanitha67@gmail.com>
2018-10-21 23:18:58 +0530

    modify ItemController

--- ShoppingCart/Areas/Shop/Controllers/ItemController.cs
+++ ShoppingCart/Areas/Shop/Controllers/ItemController.cs
@@ -11,6 +11,7 @@ using ShoppingCart.Areas.Shop.Models;
 using ShoppingCart.Models;
 using System.Net.Http.Headers;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server;
 
 namespace ShoppingCart.Areas.Shop.Controllers
 {
@@ -30,7 +31,6 @@ namespace ShoppingCart.Areas.Shop.Controllers
         // GET: Item
         public ActionResult ItemDetail(int id)
         {
-
             return View(_itemData.Details(id));
         }
 
@@ -48,7 +48,7 @@ namespace ShoppingCart.Areas.Shop.Controllers
         {
             TempData["id"] = id;
             List<ItemCategory> itemCategorieslist = new List<ItemCategory>();
-            itemCategorieslist = (from itemcategory in _db.ItemCategories.Where(x => x.SignUpId == 7) select itemcategory).ToList();
+            itemCategorieslist = (from itemcategory in _db.ItemCategories.Where(x => x.SignUpId == 7 || x.SignUpId==id) select itemcategory).ToList();
             itemCategorieslist.Insert(0, new ItemCategory { ItemCategoryId = 0, ItemCategoryName = "Select" });
             ViewBag.ListItemCategory = itemCategorieslist;
 
@@ -78,7 +78,7 @@ namespace ShoppingCart.Areas.Shop.Controllers
                     using (FileStream filestream = System.IO.File.Create(this.Getpath(filename, id, obj)))
                     {
                         files1.CopyToAsync(filestream);
-                        obj.ImageUrl1 = filename;
+                        obj.ImageUrl1 = "\\" + obj.SignUpId + "\\" + obj.ItemName + "\\" + filename;
                         
                     }
                 }
@@ -92,7 +92,7 @@ namespace ShoppingCart.Areas.Shop.Controllers
                     using (FileStream filestream = System.IO.File.Create(this.Getpath(filename, id, obj)))
                     {
                         files2.CopyToAsync(filestream);
-                        obj.ImageUrl2 = filename;
+                        obj.ImageUrl2 = "\\" + obj.SignUpId + "\\" + obj.ItemName + "\\" + filename;
                     }
                 }
                 //Third image /////////////////////////////////////////////////
@@ -105,7 +105,7 @@ namespace ShoppingCart.Areas.Shop.Controllers
                     using (FileStream filestream = System.IO.File.Create(this.Getpath(filename, id, obj)))
                     {
                         files3.CopyToAsync(filestream);
-                        obj.ImageUrl3 = filename;
+                        obj.ImageUrl3 = "\\" + obj.SignUpId + "\\" + obj.ItemName + "\\" + filename;
                     }
                 }
                 //Fourth Image ///////////////////////////////////////////////
@@ -118,7 +118,7 @@ namespace ShoppingCart.Areas.Shop.Controllers
                     using (FileStream filestream = System.IO.File.Create(this.Getpath(filename, id, obj)))
                     {
                         files4.CopyToAsync(filestream);
-                        obj.ImageUrl4 = filename;
+                        obj.ImageUrl4 = "\\" + obj.SignUpId + "\\" + obj.ItemName + "\\" + filename;
                     }
                 }
                 //foreach (IFormFile item in files)
@@ -185,7 +185,7 @@ namespace ShoppingCart.Areas.Shop.Controllers
         }
         private string Getpath(string filename, int id, ItemDTO obj)
         {
-            string path = _hostingEnvironment.WebRootPath + "\\shopImages\\" + id +"\\ItemImages\\" + obj.ItemName + "\\";
+            string path = _hostingEnvironment.WebRootPath + "\\itemImages\\" + obj.ItemCategoryId +"\\"+ obj.SignUpId +"\\" +obj.ItemName + "\\";
             if (!Directory.Exists(path))
                 Directory.CreateDirectory(path);
             return path + filename;
@@ -205,20 +205,106 @@ namespace ShoppingCart.Areas.Shop.Controllers
         public ActionResult Edit(int id, int edit)
         {
             TempData["id"] = id;
+            List<ItemCategory> itemCategorieslist = new List<ItemCategory>();
+            itemCategorieslist = (from itemcategory in _db.ItemCategories.Where(x => x.SignUpId == 7 || x.SignUpId == id) select itemcategory).ToList();
+            itemCategorieslist.Insert(0, new ItemCategory { ItemCategoryId = 0, ItemCategoryName = "Select" });
+            ViewBag.ListItemCategory = itemCategorieslist;
             return View(_itemData.EditItemId(edit));
         }
 
         // POST: Item/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(ItemDTO obj, int id)
+        public ActionResult Edit(ItemDTO obj,IFormFile files1, IFormFile files2, IFormFile files3, IFormFile files4, int id)
         {
+            var itemcategoryid = HttpContext.Request.Form["ItemCategoryId"].ToString();
+            obj.ItemCategoryId = Convert.ToInt32(itemcategoryid);
+
             if (ModelState.IsValid)
             {
-
-
                 try
                 {
+                    if (files1 != null)
+                    {
+                        var selectImageUrl = _db.Items.Where(x => x.ItemId == obj.ItemId).Select(x => x.ImageUrl1).SingleOrDefault();
+                        var completepath = _hostingEnvironment.WebRootPath + "\\itemImages\\" + obj.ItemCategoryId  + selectImageUrl;
+                        if (System.IO.File.Exists(completepath))
+                        {
+                            System.IO.File.Delete(completepath);
+                           
+                        }
+
+                        var filepath = Path.GetTempFileName();
+                        string filename = ContentDispositionHeaderValue.Parse(files1.ContentDisposition).FileName.Trim('"');
+                        filename = this.EnsureFilename(filename);
+
+                        using (FileStream filestream = System.IO.File.Create(this.Getpath(filename, id, obj)))
+                        {
+                            files1.CopyToAsync(filestream);
+                            obj.ImageUrl1 = "\\" + obj.SignUpId + "\\" + obj.ItemName + "\\" + filename;
+
+                        }
+                    }
+                    //Second Image ////////////////////////////////////////////////
+                    if (files2 != null)
+                    {
+                        var selectImageUrl = _db.Items.Where(x => x.ItemId == obj.ItemId).Select(x => x.ImageUrl2).SingleOrDefault();
+                        var completepath = _hostingEnvironment.WebRootPath + "\\itemImages\\" + obj.ItemCategoryId + selectImageUrl;
+                        if (System.IO.File.Exists(completepath))
+                        {
+                            System.IO.File.Delete(completepath);
+
+                        }
+                        var filepath = Path.GetTempFileName();
+                        string filename = ContentDispositionHeaderValue.Parse(files2.ContentDisposition).FileName.Trim('"');
+                        filename = this.EnsureFilename(filename);
+
+                        using (FileStream filestream = System.IO.File.Create(this.Getpath(filename, id, obj)))
+                        {
+                            files2.CopyToAsync(filestream);
+                            obj.ImageUrl2 = "\\" + obj.SignUpId + "\\" + obj.ItemName + "\\" + filename;
+                        }
+                    }
+                    //Third image /////////////////////////////////////////////////
+                    if (files3 != null)
+                    {
+                        var selectImageUrl = _db.Items.Where(x => x.ItemId == obj.ItemId).Select(x => x.ImageUrl3).SingleOrDefault();
+                        var completepath = _hostingEnvironment.WebRootPath + "\\itemImages\\" + obj.ItemCategoryId + selectImageUrl;
+                        if (System.IO.File.Exists(completepath))
+                        {
+                            System.IO.File.Delete(completepath);
+
+                        }
+                        var filepath = Path.GetTempFileName();
+                        string filename = ContentDispositionHeaderValue.Parse(files3.ContentDisposition).FileName.Trim('"');
+                        filename = this.EnsureFilename(filename);
+
+                        using (FileStream filestream = System.IO.File.Create(this.Getpath(filename, id, obj)))
+                        {
+                            files3.CopyToAsync(filestream);
+                            obj.ImageUrl3 = "\\" + obj.SignUpId + "\\" + obj.ItemName + "\\" + filename;
+                        }
+                    }
+                    //Fourth Image ///////////////////////////////////////////////
+                    if (files4 != null)
+                    {
+                        var selectImageUrl = _db.Items.Where(x => x.ItemId == obj.ItemId).Select(x => x.ImageUrl4).SingleOrDefault();
+                        var completepath = _hostingEnvironment.WebRootPath + "\\itemImages\\" + obj.ItemCategoryId + selectImageUrl;
+                        if (System.IO.File.Exists(completepath))
+                        {
+                            System.IO.File.Delete(completepath);
+
+                        }
+                        var filepath = Path.GetTempFileName();
+                        string filename = ContentDispositionHeaderValue.Parse(files4.ContentDisposition).FileName.Trim('"');
+                        filename = this.EnsureFilename(filename);
+
+                        using (FileStream filestream = System.IO.File.Create(this.Getpath(filename, id, obj)))
+                        {
+                            files4.CopyToAsync(filestream);
+                            obj.ImageUrl4 = "\\" + obj.SignUpId + "\\" + obj.ItemName + "\\" + filename;
+                        }
+                    }
                     // TODO: Add update logic here
                     _itemData.EditedItem(obj);
                     return RedirectToAction("Index", "Shop", new { id = id});
@@ -233,8 +319,9 @@ namespace ShoppingCart.Areas.Shop.Controllers
 
         // GET: Item/Delete/5
         [HttpGet]
-        public ActionResult Delete(int id, int delete)
+        public ActionResult Delete(int id, int delete, int categoryid)
         {
+            
             TempData["id"] = id;
             return View(_itemData.GetDeleteItem(delete));
         }
@@ -249,6 +336,14 @@ namespace ShoppingCart.Areas.Shop.Controllers
             {
                 try
                 {
+                    var categoryId = _db.Items.Where(x => x.ItemId == ItemId).Select(x => x.ItemCategoryId).SingleOrDefault();
+                    var itemName = _db.Items.Where(x => x.ItemId == ItemId).Select(x => x.ItemName).SingleOrDefault();
+                    var completepath = _hostingEnvironment.WebRootPath + "\\itemImages\\" + categoryId + "\\" + id +"\\"+ itemName +"\\";
+                    if (System.IO.File.Exists(completepath))
+                    {
+                        System.IO.File.Delete(completepath);
+
+                    }
 
                     _itemData.Delete(ItemId);
                     return RedirectToAction("Index", "Shop", new { id = id});
